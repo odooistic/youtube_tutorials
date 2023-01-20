01323e8d80520a00c2a02f5af2f1be50643bc912
ffaemy <odooistic@gmail.com>
2023-01-20 18:21:26 +0000

    numerous tutorials code
    numerous tutorials code

---
+++ how to create a module in odoo16/customization/__manifest__.py
@@ -0,0 +1,20 @@
+{
+
+    'name': 'General Customization',
+    'version': '1.0.0.0.0',
+    'summary': 'General Customization',
+    'sequence': 30,
+    'description': """
+    Long Description of your modules""",
+    'author': 'Odooistic',
+    'company': 'Company Name',
+    'website': 'https://www.website.com',
+    'category': 'Customization',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    
+
+   ],
+    'application': True,
+}

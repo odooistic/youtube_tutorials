ddeddf43aab59d99325dcad308a0ab60acbe2346
ffaemy <odooistic@gmail.com>
2023-08-05 14:01:34 +0100

    task highlight
    Easy Task Highlighting in Odoo: Change Colors with a Click!"

---
+++ easy task highlighting ion odoo/project_task.py
@@ -0,0 +1,79 @@
+from odoo import models, fields, api, _
+
+class ProductTemplate(models.Model):
+    _inherit= "product.template"
+    
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        res = super(ProductTemplate, self).copy(default=default)
+        res.write({
+            'default_code': self.default_code
+        })
+
+        for seller in self.seller_ids:
+            seller.copy({
+                'product_tmpl_id': res.id
+            })
+        return res
+    
+
+class ProjectTask(models.Model):
+    _inherit= "project.task"
+    
+    color = fields.Integer(compute='_compute_color', store=True)
+    change_color = fields.Boolean(string='Change Color')
+    
+    @api.depends('change_color')
+    def _compute_color(self):
+        for record in self:
+            if record.change_color:
+                record.color = 9
+            else:
+                record.color = 0
+                
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # to_fix = fields.Boolean(string='Fix')
+    # name = fields.Char(string='Name', compute='_get_task_name', store=True)
+    
+    # @api.depends('to_fix', 'name')
+    # def _get_task_name(self):
+    #     for record in self:
+    #         if record.to_fix:
+    #             record.name = ('[FIX]' + record.name)
+    #         else:
+    #             record.name = record.name.replace('[FIX]', '')
+    
+    
+                
+    
+    
+    
+  
+        
